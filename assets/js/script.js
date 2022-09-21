0790747ec9eabee6ee2eb74980dcfb6ec018758c
devWeibel <112207453+devWeibel@users.noreply.github.com>
2022-09-20 22:53:08 -0400

    added html and css for question screens

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// toggle between hiding and showing element
+
+function myFunction() {
+    var x = document.getElementById('myDiv');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
