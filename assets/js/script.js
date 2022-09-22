5ab6a6bccbddc714a8a79767b49400ebaa66076a
devWeibel <112207453+devWeibel@users.noreply.github.com>
2022-09-21 23:33:46 -0400

    cleaned up some JS and CSS. added comments to HTML and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,58 @@
-// toggle between hiding and showing element
-
 var startButton = document.getElementById("start-button");
 var i = 0;
+
+//Array to hold objects for each question and answer choices w/ correct answer identified:
 var questions = [
     {
-        text: "question 1",
-        choices: ["answer 1", "answer 2", "answer 3", "answer 4"],
-        correct: "answer 1"
+        text: "How do you create a function in JavaScript?",
+        answers: ["1) function myFunction()", "2) function = myFunction()", "3) function:myFunction()", "4) myFunction()"],
+        correct: "1) function myFunction()"
     },
     {
-        text: "question 2",
-        choices: ["answer 1", "answer 2", "answer 3", "answer 4"],
-        correct: "answer 1"
+        text: "How do you write an IF statement in JavaScript?",
+        answers: ["1) if i == 5 then", "2) if i = 5", "3) if i = 5 then", "4) if (i == 5)"],
+        correct: "4) if (i == 5)"
     },
     {
-        text: "question 3",
-        choices: ["answer 1", "answer 2", "answer 3", "answer 4"],
-        correct: "answer 1"
+        text: "How does a FOR loop start?",
+        answers: ["1) for (i = 0; i <= 5; i++)", "2) for i = 1 to 5", "3) for (i = 0; i <= 5)", "4) for (i <= 5; i++)"],
+        correct: "1) for (i = 0; i <= 5; i++)"
     },
     {
-        text: "question 4",
-        choices: ["answer 1", "answer 2", "answer 3", "answer 4"],
-        correct: "answer 1"
+        text: "How can you add a comment in JavaScript?",
+        answers: ["1) /*This is a comment.*/", "2) <!--This is a comment.-->", "3) $This is a comment.", "4) //This is a comment."],
+        correct: "4) //This is a comment."
     },
     {
-        text: "question 5",
-        choices: ["answer 1", "answer 2", "answer 3", "answer 4"],
-        correct: "answer 1"
+        text: "How do you declare a JavaScript variable?",
+        answers: ["1) v catName;", "2) variable catName;", "3) var catName;", "4) var = catName;"],
+        correct: "3) var catName;"
     },
 ]
 
+//Called on 'Start Quiz' button click - hides quiz instructions and calls buildQuestions() to start the question portion: 
 function startGame() {
     document.getElementById("instruction-box").setAttribute("class", "hide");
-
     document.getElementById("question-box").removeAttribute("class");
-
-    buildQuestionContainer();
+    buildQuestions();
 }
 
-function buildQuestionContainer() {
-    // ask 1 question. loop over choices and set values on buttons. append buttons to page. check answer correct or wrong. ask next question. 
-    document.getElementById("question").textContent = questions[i].text
-
+//Populates the #question id with questions array index[i]. Clears any pre-existing answer buttons in #button-box. 
+function buildQuestions() {
+    document.getElementById("question").textContent = questions[i].text;
     document.getElementById("button-box").innerHTML = "";
-
-    questions[i].choices.forEach(function (choice) {
+//Creates buttons with text of answerChoice and class .answer for each 'answers' element within index[i]. Adds the buttons to #button-box. Calls checkAnswer() on button click.
+    questions[i].answers.forEach(function (answerChoice) {
         var button = document.createElement("button");
         button.setAttribute("class", "answer");
-        button.textContent = choice;
-        button.setAttribute("value", choice);
-        button.onclick = checkAnswer;
+        button.textContent = answerChoice;
+        button.setAttribute("value", answerChoice);
         document.getElementById("button-box").append(button);
+        button.onclick = checkAnswer;
     })
 }
 
+//checks if the clicked answer button matches the correct answer then increments i.
 function checkAnswer() {
     console.log(this.value);
     if (this.value !== questions[i].correct) {
@@ -62,31 +61,13 @@ function checkAnswer() {
         console.log("correct");
     } 
     i++;
-
+//Checks if the current question [i] was the last and either builds the next question or ends the game.
     if (i === questions.length) {
         console.log("end game");
     } else {
-        buildQuestionContainer();
+        buildQuestions();
     }
 }
 
 
-
-
-
-
-
 startButton.addEventListener("click", startGame);
-
-
-// function myFunction() {
-//     var x = document.getElementById('myDiv');
-//     if (x.style.display === 'none') {
-//         x.style.display = 'block';
-//     } else {
-//         x.style.display = 'none';
-//     }
-// }
-
-
-
